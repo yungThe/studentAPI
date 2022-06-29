d5c3f2240402f1d2e57adad694fe8394ac2f0f40
yungThe <74132467+yungThe@users.noreply.github.com>
2022-06-29 15:24:42 +0700

    update

--- routes/student.js
+++ routes/student.js
@@ -5,191 +5,191 @@ import nodemailer from 'nodemailer';
 const router = express.Router();
 const students = [
     {
-        id : "1",
-        lastname : "Nguyen",
-        firstName : "Dung",
-        year : "4th",
-        total : "8000000",
+        id: "1",
+        lastname: "Nguyen",
+        firstName: "Dung",
+        year: "4th",
+        total: "8000000",
+        status: "0",
+        email: "longdv265@gmail.com"
+    },
+    {
+        id: "2",
+        lastname: "Do",
+        firstName: "Anh",
+        year: "2th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "2",
-        lastname : "Do",
-        firstName : "Anh",
-        year : "2th",
-        total : "8000000",
+        id: "3",
+        lastname: "Nguyen",
+        firstName: "Tung",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "3",
-        lastname : "Nguyen",
-        firstName : "Tung",
-        year : "4th",
-        total : "8000000",
+        id: "4",
+        lastname: "Nguyen",
+        firstName: "Long",
+        year: "3th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "4",
-        lastname : "Nguyen",
-        firstName : "Long",
-        year : "3th",
-        total : "8000000",
+        id: "5",
+        lastname: "Bui",
+        firstName: "An",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "5",
-        lastname : "Bui",
-        firstName : "An",
-        year : "4th",
-        total : "8000000",
+        id: "6",
+        lastname: "Luong",
+        firstName: "Tung",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "6",
-        lastname : "Luong",
-        firstName : "Tung",
-        year : "4th",
-        total : "8000000",
+        id: "7",
+        lastname: "Do",
+        firstName: "Hung",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "7",
-        lastname : "Do",
-        firstName : "Hung",
-        year : "4th",
-        total : "8000000",
+        id: "8",
+        lastname: "Nguyen",
+        firstName: "Bao",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "8",
-        lastname : "Nguyen",
-        firstName : "Bao",
-        year : "4th",
-        total : "8000000",
+        id: "9",
+        lastname: "Nguyen",
+        firstName: "Huyen",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "9",
-        lastname : "Nguyen",
-        firstName : "Huyen",
-        year : "4th",
-        total : "8000000",
+        id: "10",
+        lastname: "Nguyen",
+        firstName: "Tung",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "10",
-        lastname : "Nguyen",
-        firstName : "Tung",
-        year : "4th",
-        total : "8000000",
+        id: "11",
+        lastname: "Nguyen",
+        firstName: "Huy",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "11",
-        lastname : "Nguyen",
-        firstName : "Huy",
-        year : "4th",
-        total : "8000000",
+        id: "12",
+        lastname: "Nguyen",
+        firstName: "Khang",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "12",
-        lastname : "Nguyen",
-        firstName : "Khang",
-        year : "4th",
-        total : "8000000",
+        id: "13",
+        lastname: "Nguyen",
+        firstName: "Khanh",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "13",
-        lastname : "Nguyen",
-        firstName : "Khanh",
-        year : "4th",
-        total : "8000000",
+        id: "14",
+        lastname: "Nguyen",
+        firstName: "Sang",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "14",
-        lastname : "Nguyen",
-        firstName : "Sang",
-        year : "4th",
-        total : "8000000",
+        id: "15",
+        lastname: "Bach",
+        firstName: "Ha",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "15",
-        lastname : "Bach",
-        firstName : "Ha",
-        year : "4th",
-        total : "8000000",
+        id: "16",
+        lastname: "Nguyen",
+        firstName: "Tu",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "16",
-        lastname : "Nguyen",
-        firstName : "Tu",
-        year : "4th",
-        total : "8000000",
+        id: "17",
+        lastname: "Do",
+        firstName: "Nam",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "17",
-        lastname : "Do",
-        firstName : "Nam",
-        year : "4th",
-        total : "8000000",
+        id: "18",
+        lastname: "Hoang",
+        firstName: "Nam",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "18",
-        lastname : "Hoang",
-        firstName : "Nam",
-        year : "4th",
-        total : "8000000",
+        id: "19",
+        lastname: "Duong",
+        firstName: "Dung",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "19",
-        lastname : "Duong",
-        firstName : "Dung",
-        year : "4th",
-        total : "8000000",
+        id: "20",
+        lastname: "Ha",
+        firstName: "Son",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
     {
-        id : "20",
-        lastname : "Ha",
-        firstName : "Son",
-        year : "4th",
-        total : "8000000",
-        status: "0",
-        email: "mail@gmail.com"
-    },
-    {
-        id : "21",
-        lastname : "Nguyen",
-        firstName : "Vu",
-        year : "4th",
-        total : "8000000",
+        id: "21",
+        lastname: "Nguyen",
+        firstName: "Vu",
+        year: "4th",
+        total: "8000000",
         status: "0",
         email: "mail@gmail.com"
     },
@@ -197,53 +197,53 @@ const students = [
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'thedungptit1@gmail.com',
-      pass: 'raxhmehcvfojlgqz'
+        user: 'thedungptit1@gmail.com',
+        pass: 'raxhmehcvfojlgqz'
     }
-  });
-  
-  var messageConfirm = "<h1> Xác nhận đã nộp học phí</h1>"
-  var msgReminder = "<h1>Nộp thiếu, lưu ý nộp đủ trước thời hạn</h1>"
-  var message = "<h1> Thông báo bắt đầu nhận học phí kì 20221, kết thúc sau 24h</h1>"
-  
-  var mailevent = {
+});
+
+var messageConfirm = "<h1> Xác nhận đã nộp học phí</h1>"
+var msgReminder = "<h1>Nộp thiếu, lưu ý nộp đủ trước thời hạn</h1>"
+var message = "<h1> Thông báo bắt đầu nhận học phí kì 20221, kết thúc sau 24h</h1>"
+
+var mailevent = {
     from: 'thedungptit1@gmail.com',
     to: 'dungthailand120800@gmail.com',
     subject: 'Thông báo thu học phí kì học 20221',
     html: message
-  };
-  
-  
-  var mail1 = {
+};
+
+
+var mail1 = {
     from: 'thedungptit1@gmail.com',
     to: 'dungthailand120800@gmail.com',
     subject: 'Học phí kì học 20221',
     html: messageConfirm
-  };
-  var mail2 = {
+};
+var mail2 = {
     from: 'thedungptit1@gmail.com',
     to: 'dungthailand120800@gmail.com',
     subject: 'Reminder học phí kì học 20221',
     html: msgReminder
-  };
-  
-  transporter.sendMail(mailevent, function(error, info){
+};
+
+transporter.sendMail(mailevent, function (error, info) {
     if (error) {
-      console.log(error);
+        console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+        console.log('Email sent: ' + info.response);
     }
-  });
+});
 
 
 
 
 
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
     res.send(students);
 });
 
-router.post('/',(req,res) =>{
+router.post('/', (req, res) => {
     const student = req.body;
     const studentsID = req.body.id;
     const studentWithID = { ...student, id: studentsID }
@@ -251,38 +251,41 @@ router.post('/',(req,res) =>{
     res.send('Student added');
 });
 
-router.get('/:id', (req,res) =>{
-    const {id} = req.params;
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
     const result = students.find((students) => students.id === id);
     res.send(result);
 });
 
-router.put('/edit/:id', (req, res) =>{
+router.put('/edit/:id', (req, res) => {
     const { id } = req.params;
-    const { total } = req.body;
+    const {money} = req.body;
+    console.log(money);
     const result = students.find((students) => students.id === id);
-    
-    if(total < result.total){
-        transporter.sendMail(mail2, function(error, info){
+
+    if (money < result.total) {
+        result.total -= money;
+        transporter.sendMail(mail2, function (error, info) {
             if (error) {
-              console.log(error);
+                console.log(error);
             } else {
-              console.log('Email sent: ' + info.response);
+                console.log('Email sent: ' + info.response);
             }
-          });
+        });
     }
-    if(total >= result.total){
+    else if (money >= result.total) {
+        result.total = 0;
         result.status = 1;
-        transporter.sendMail(mail1, function(error, info){
+        transporter.sendMail(mail1, function (error, info) {
             if (error) {
-              console.log(error);
+                console.log(error);
             } else {
-              console.log('Email sent: ' + info.response);
+                console.log('Email sent: ' + info.response);
             }
-          });
+        });
     }
     res.send(result);
-    
+
 });
 
 /*
@@ -293,7 +296,7 @@ router.get('/:invoice' ,(req, res) => {
 });
 */
 
-router.get('/invoice/:id' ,(req, res) => {
+router.get('/invoice/:id', (req, res) => {
     const { id } = req.params;
     const result = students.find((students) => students.id === id);
     res.send(result)
